58f26aac0d1b6f22d3f1dde63e3611b959d3b5be
alinizam <alinizam99@gmail.com>
2018-10-04 19:00:45 +0300

    Giriş Dersi

---
+++ BLM104_2018_2019_Guz/src/Ders02_Lab/Ornek1/Personel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ders02_Lab.Ornek1;
+
+/**
+ *
+ * @author anizam
+ */
+public class Personel {
+    public String Adi;
+    public String Soyadi;
+    public String Birim;
+    
+}
---
+++ BLM104_2018_2019_Guz/src/Ders02_Lab/Ornek1_2/Personel.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ders02_Lab.Ornek1_2;
+
+/**
+ *
+ * @author anizam
+ */
+public class Personel {
+    
+    public String adi;
+    public String soyAdi;
+    public String birim;
+    
+    
+    
+}
